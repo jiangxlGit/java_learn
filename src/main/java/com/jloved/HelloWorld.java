604f73d88715d82cb4b09bdc91e1ab9451b3b357
jiangxl <jiangxl@163.com>
2024-07-09 16:45:35 +0800

    新增hello world类

---
+++ src/main/java/com/jloved/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.jloved;
+
+/**
+ * @author jiangxl
+ * @version V1.0
+ * @Description TODO
+ * @ClassName HelloWorld
+ * @Date 2024/7/9 下午4:41
+ */
+public class HelloWorld {
+
+    // hello world
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
